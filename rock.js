02477a394d8327ac40e6bc4c571647219ae92918
MarciHuMex <151879261+MarciHuMex@users.noreply.github.com>
2024-01-18 21:59:41 +0100

    Make computer a random chouce of rock paper scissors

--- rock.js
+++ rock.js
@@ -1,1 +1,8 @@
-console.log("Hola mundo");
+function getComputerChoice (a,b,c) {
+    var choices = [a,b,c];
+    var randomChoice = Math.floor(Math.random()*choices.length);
+    return choices[randomChoice];
+}
+
+var computerChoice = getComputerChoice("Rock", "Paper", "Scissors");
+console.log("Computer's choice: " + computerChoice);
