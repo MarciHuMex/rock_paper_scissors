7c54ef31e3a451a5977f6752708efd4bcf7fd89a
MarciHuMex <151879261+MarciHuMex@users.noreply.github.com>
2024-01-19 20:46:44 +0100

    Add playerselection function still without ndeclaring winner

--- rock.js
+++ rock.js
@@ -1,8 +1,20 @@
-function getComputerChoice (a,b,c) {
-    var choices = [a,b,c];
+function getComputerChoice () {
+    var choices = ["Rock", "Paper", "Scissors"];
     var randomChoice = Math.floor(Math.random()*choices.length);
     return choices[randomChoice];
 }
 
-var computerChoice = getComputerChoice("Rock", "Paper", "Scissors");
+var computerChoice = getComputerChoice();
 console.log("Computer's choice: " + computerChoice);
+
+function getPlayerSelection() {
+    const input = prompt("Choose Rock, Paper or Scissors:")
+    if(input) {
+        return input.toLowerCase().charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+    } else {
+        return null;
+    }
+}
+
+var playerChoice = getPlayerSelection();
+console.log(playerChoice);
